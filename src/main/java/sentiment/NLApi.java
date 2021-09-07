1377680a465d5c72f0879117d23bbdc706fd4e75
unknown <nraisane@asu.edu>
2021-09-06 17:27:44 -0700

    Natural Language API functional with score and magnitude

--- src/main/java/sentiment/NLApi.java
+++ src/main/java/sentiment/NLApi.java
@@ -9,30 +9,42 @@ import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
 
 public class NLApi {
-	
-	public static String getSentiment(String content) {
-	    try (LanguageServiceClient language = LanguageServiceClient.create()) {
 
-		      // The text to analyze
-		      String text = content;
-		      Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
-		      System.out.println(doc);
+	public static String[] getSentiment(String content) {
+		try (LanguageServiceClient language = LanguageServiceClient.create()) {
+			// The text to analyze
+			String text = content;
+			Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+			System.out.println(doc);
 
-		      // Detects the sentiment of the text
-		      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
-		      System.out.println(sentiment);
-		      System.out.printf("Text: %s%n", text);
-		      System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
-		      String strScore = Float.toString(sentiment.getScore());
-		      return strScore;
-		    } catch (IOException e) {
-		    	e.printStackTrace();
-		    } // end try block
-	    return "hi";
+			// Detects the sentiment of the text
+			Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+			System.out.println(sentiment);
+			System.out.printf("Text: %s%n", text);
+			System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
+			String[] sentimentArray = new String[] {Float.toString(sentiment.getScore()), Float.toString(sentiment.getMagnitude())};
+			return sentimentArray;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // end try block
+		
+		// Placeholder to satisfy return requirement
+		String[] ph = new String[] {};
+		return ph;
 	}
+
+	public static String[] getTweetInfo() {
+
+		// Below 2 lines are placeholder
+		String[] ph = new String[] { "a", "b" };
+		return ph;
+	}
+
 	public static void main(String[] args) {
-		String sentimentRating = getSentiment("WOW, I love pie it is SOOOO good and amazing!!!");
-		System.out.printf("This is the sentiment score: %s", sentimentRating);
+		String[] sResult = getSentiment("test I am very happy :D");
+		String sScore = sResult[0];
+		String sMagnitude = sResult[1];
+		System.out.printf("Score: %s\nMagnitude: %s", sScore, sMagnitude);
 
 	}
 
