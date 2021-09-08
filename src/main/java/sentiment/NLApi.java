bd7e6a6b2d7aa6fe886ed70ad15dbea99c2de1da
unknown <nraisane@asu.edu>
2021-09-07 19:44:36 -0700

    functioning twitter API call to json

--- src/main/java/sentiment/NLApi.java
+++ src/main/java/sentiment/NLApi.java
@@ -7,6 +7,13 @@ import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
+import com.google.gson.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class NLApi {
 
@@ -33,19 +40,50 @@ public class NLApi {
 		return ph;
 	}
 
-	public static String[] getTweetInfo() {
-
+	public static String[] getTweetInfo(String searchTerm) {
+		String tweetsApi = "https://api.twitter.com/1.1/search/tweets.json";
+		// Adding search term and authentication
+		tweetsApi += "?q=" + searchTerm + "&count=1";
+		System.out.println("Getting info from: " + tweetsApi);
+		String twitterBearer = System.getenv("TWITTER_BEARER");
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder(
+				URI.create(tweetsApi))
+				.header("Authorization", twitterBearer)
+				.build();
+		
+		// Get response object
+		HttpResponse<String> response = null;
+		try {
+		response = client.send(request, BodyHandlers.ofString());
+		System.out.print("Response body: " + response.body());
+		System.out.println("\nResponse.toString(): " + response.toString());
+		System.out.println("response.request(): " + response.request());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Gson gson = new Gson();
+		JsonObject body = gson.fromJson(response.body(), JsonObject.class);
+		JsonArray statuses = body.get("statuses").getAsJsonArray();
+		JsonObject firstResult = statuses.get(0).getAsJsonObject();
+		JsonElement tweetText = firstResult.get("text");
+		
+		
+		
+		System.out.printf("Here is json formatted tweet text: %s", tweetText.getAsString());
 		// Below 2 lines are placeholder
 		String[] ph = new String[] { "a", "b" };
 		return ph;
 	}
 
 	public static void main(String[] args) {
-		String[] sResult = getSentiment("test I am very happy :D");
-		String sScore = sResult[0];
-		String sMagnitude = sResult[1];
-		System.out.printf("Score: %s\nMagnitude: %s", sScore, sMagnitude);
-
+//		String[] sResult = getSentiment("test I am very happy :D");
+//		String sScore = sResult[0];
+//		String sMagnitude = sResult[1];
+//		System.out.printf("Score: %s\nMagnitude: %s", sScore, sMagnitude);
+		getTweetInfo("Spiderman");
+		System.out.print("\nDone executing");
 	}
 
 }
