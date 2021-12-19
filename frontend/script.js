168e71eb8170edfb117644cc1824f606f27f140f
unknown <nraisane@asu.edu>
2021-12-19 14:10:56 -0700

    cleaned up script imports

--- frontend/script.js
+++ frontend/script.js
@@ -4,19 +4,19 @@ function stringify_result(score, searchTerm) {
     var statement;
     if (score >= 0.3) {
         keyword = "Very Positive";
-        statement = `Looks like the people of Twitter are quite fond of "${searchTerm}"`;
+        statement = `Looks like the people of Twitter are quite fond of "${searchTerm}"...`;
     } else if (score >= 0.1) {
         keyword = "Slightly Positive";
-        statement = `Looks like the people of Twitter kind of like "${searchTerm}"`;
+        statement = `Looks like the people of Twitter kind of like "${searchTerm}"...`;
     } else if (score <= -0.3) {
         keyword = "Very Negative";
-        statement = `Looks like the people of Twitter are repulsed by "${searchTerm}"`;
+        statement = `Looks like the people of Twitter REALLY don't like "${searchTerm}"...`;
     } else if (score <= -0.1) {
         keyword = "Slightly Negative";
-        statement = `Looks like the people of Twitter have a distaste for "${searchTerm}"`;
+        statement = `Looks like the people of Twitter aren't fans of "${searchTerm}"...`;
     } else {
         keyword = "Neutral";
-        statement = `Looks like the people of Twitter are indifferent about "${searchTerm}"`
+        statement = `Looks like the people of Twitter are indifferent about "${searchTerm}"...`
     }
     return [keyword, statement];
 }
