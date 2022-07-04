35a4c0ad05a534905bf48b7c95f1cc75a0f4437f
unknown <nraisane@asu.edu>
2022-07-04 13:32:30 -0700

    add text to landing page

--- twitter-sentiment-analysis/src/App.js
+++ twitter-sentiment-analysis/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TopNav from './components/TopNav';
+import Header from './components/Header';
+import MainText from './components/MainText';
 
 function App() {
   return (
     <div className='App'>
       <TopNav />
+      <Header />
+      <MainText />
     </div>
   );
 }
---
+++ twitter-sentiment-analysis/src/components/MainText.js
@@ -0,0 +1,26 @@
+import Button from 'react-bootstrap/Button';
+import {Container, Row, Col} from 'react-bootstrap';
+import './MainText.css';
+
+
+function MainText() {
+    return(
+        <Container>
+            <Row>
+                <Col md="6" className='columnStyle'>
+                <p>
+                    Perform a search on any word and find out how the people of Twitter feel about it. In a world of biased sources, querying the thoughts of everyday people can lead to interesting insights
+                </p>
+                </Col>
+                <Col md="6" className='columnStyle'>
+                <p>
+                    Test your assumptions with this app, you might be surprised
+                    <a href="https://github.com/NikoRaisanen/twitter-sentiment-analysis/blob/main/README.md" className='link mt-5'>How it works &gt;</a>
+                </p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default MainText;
