53e31485c2da458796f8ac1e026feb63015e31f8
unknown <nraisane@asu.edu>
2022-07-16 17:16:55 -0700

    finished search bar, added inner padding. Auto scroll to ResultGraph component

--- twitter-sentiment-analysis/src/components/SearchPage.jsx
+++ twitter-sentiment-analysis/src/components/SearchPage.jsx
@@ -1,21 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import TopNav from './TopNav';
 import ResultGraph from './ResultGraph';
 import {stringifyResult, sentimentToPercentage, createResultGraph} from '../helpers';
 import search from '../media/search-outline.svg';
 import './searchPage.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCoffee, faSearch} from '@fortawesome/free-solid-svg-icons';
+import { faCropSimple, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 function SearchPage() {
     const [searchTerm, setSearchTerm] = useState('');
     const [showResults, setShowResults] = useState(false);
     const [result, setResult] = useState({});
+    const useResultRef = useRef(null);
+
+    useEffect(() => {
+        console.log('useEffect');
+        if (showResults) {
+            scrollTo(useResultRef);
+        }
+        
+    }, [showResults])
 
     const handleChange = (e) => {
         setSearchTerm(e.target.value);
     }
+
+
+    const scrollTo = (ref) => {
+        ref.current.scrollIntoView({behavior: 'smooth'});
+    }
+
 
     const handleSubmit = async (e) => {
         // make api call and return data
@@ -23,6 +39,10 @@ function SearchPage() {
         const res = await fetch(`https://92ctge8hl1.execute-api.us-east-2.amazonaws.com/prod/sentiment-analysis?searchTerm=${searchTerm}&searchType=mixed`)
         setResult(await res.json());
         setShowResults(true);
+        // setTimeout(() => {
+        //     scrollTo(useResultRef);
+        // } , 2000);
+        // scrollTo(useResultRef);
     }
 
     return (
@@ -41,13 +61,13 @@ function SearchPage() {
         </div> */}
         <div className="container">
 
-            <div className="row height d-flex justify-content-center align-items-center">
+            <div className="row search-bar d-flex justify-content-center align-items-center">
 
               <div className="col-md-6">
 
                 <form onSubmit={handleSubmit}>
                 {/* <span className='wrapper'> */}
-                <FontAwesomeIcon className='leftIcon' icon={faCoffee}></FontAwesomeIcon>
+                <FontAwesomeIcon className='leftIcon' icon={faTwitter}></FontAwesomeIcon>
                   <input type="text" className="form-control form-input" value={searchTerm} onChange={handleChange} placeholder="Search anything..."/>
                   <span className="left-pan"><FontAwesomeIcon onClick={handleSubmit} icon={faSearch}></FontAwesomeIcon></span>
                 {/* </span> */}
@@ -66,13 +86,13 @@ function SearchPage() {
                 <input type="text" value={searchTerm} onChange={handleChange}/>
             <input type="submit" value="Search" />
         </form> */}
-        <h1>Searching for: {searchTerm}</h1>
+        {/* <h1>Searching for: {searchTerm}</h1> */}
         </div>
         {showResults && 
-            <>
-            <ResultGraph sentiment={sentimentToPercentage(result.finalSentiment)}/>
+            <div className='resultDiv' ref={useResultRef}>
+            <ResultGraph onShow={() => {console.log('showing')}} sentiment={sentimentToPercentage(result.finalSentiment)}/>
             <p>{stringifyResult(result.finalSentiment, searchTerm).statement}</p>
-            </>
+            </div>
         }
         </>
     );
